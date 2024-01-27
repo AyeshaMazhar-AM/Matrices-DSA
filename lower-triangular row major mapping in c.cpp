39090b8a8ffc5009eee04d14af1307bf4d306438
AYESHA MAZHAR <123892280+AyeshaMazhar-AM@users.noreply.github.com>
2024-01-27 16:32:39 +0500

    Add files via upload

---
+++ lower-triangular row major mapping in c.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct Matrix{
+	int *A;
+	int n;
+};
+
+    //in order to modify matrix we will take pointer
+void Set(struct Matrix *m, int i, int j, int x){
+	if(i>=j)
+	m->A[i*(i-1)/2+j-1]=x;
+}
+
+int get(struct Matrix m, int i, int j){
+	if(i>=j)
+	return m.A[i*(i-1)/2+j-1];
+	else
+	return 0;
+}
+
+void Display(struct Matrix m){
+	int i,j;
+	for(i=1; i<=m.n; i++){
+		for(j=1; j<=m.n; j++){
+			if(i>=j)
+			cout<<" "<<m.A[i*(i-1)/2+j-1];
+			else
+			cout<<" 0";
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	struct Matrix m;
+	int i,j,x;
+	
+	cout<<"enter dimensions: ";
+	cin>>m.n;
+	
+	m.A=(int *)malloc(m.n*(m.n-1)/2*sizeof(int));
+	cout<<"enter all elements: ";
+	for (i=1;i<=m.n; i++){
+		for(j=1; j<=m.n; j++){
+			cin>>x;
+			Set(&m,i,j,x);
+		}
+	}
+	
+	cout<<endl<<endl;
+	Display(m);
+}
+
